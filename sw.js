8a9bc12bfe8ff4cfeb37f200f55a5b05d6560eca
Swarnim Kalra <139457998+kalra-swarnim@users.noreply.github.com>
2026-09-22 01:20:56 +0530

    NEW_UP

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ltm-gym-v1";
+const CACHE = "ltm-gym-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
